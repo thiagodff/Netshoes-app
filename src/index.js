6b1b7ed8b3c2eed55e53741261b2f1449b186d70
thiagofd <thiago.fdornelles@gmail.com>
2019-12-01 21:29:51 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, StatusBar } from 'react-native';
+
+function App() {
+  return (
+    <>
+      <StatusBar backgroundColor="#5b2d83" barStyle="light-content" />
+      <Text>Home</Text>
+    </>
+  );
+}
+
+export default App;
